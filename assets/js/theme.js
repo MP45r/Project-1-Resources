b85168073f446af18d69dec9aeedd1d43e68af48
MP45r <101152692+MP45r@users.noreply.github.com>
2022-06-10 22:40:22 -0500

    Update theme.js

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,6 +3,8 @@ const body = document.body
 const lightBulb = document.getElementById('light-bulb')
 const lightBulbState = document.getElementById('light-bulb-state')
 const header = document.getElementById('header-id-1')
+const blogPosts = document.getElementById('blog-posts')
+const about = document.getElementById('about')
 const changeTheme = () => {
     isDark ? (isDark = false) : (isDark = true)
     isDark ? (localStorage.setItem('isDark', true)) : (localStorage.setItem('isDark', false))
@@ -13,19 +15,32 @@ const changeTheme = () => {
 const themeT = (t) => {
     let addC = t ? 'dark-theme' : 'light-theme'
     let removeC = t ? 'light-theme' : 'dark-theme'
+    //
     let addHeaderC = t ? 'header-dark' : 'header-light'
     let removeHeaderC = t ? 'header-light' : 'header-dark'
+    //
     let lightBulbTitle = t ? 'Dark Mode is On' : 'Light Mode is On'
+    //
+    let addBlogC = t ? 'blog-posts-dark' : 'blog-posts-light'
+    let removeBlogC = t ? 'blog-posts-light' : 'blog-posts-dark'
+    //
+    let addAboutC = t ? 'about-dark' : 'about-light'
+    let removeAboutC = t ? 'about-light' : 'about-dark'
+    //
     let addLightBulbC = t ? 'light-bulb-dark' : 'light-bulb-light'
     let removeLightBulbC = t ? 'light-bulb-light' : 'light-bulb-dark'
     //remove
     body.classList.remove(removeC)
     lightBulb.classList.remove(removeLightBulbC)
     header.classList.remove(removeHeaderC)
+    blogPosts.classList.remove(removeBlogC)
+    about.classList.remove(removeAboutC)
     //add
     body.classList.add(addC)
     lightBulb.classList.add(addLightBulbC)
     header.classList.add(addHeaderC)
+    blogPosts.classList.add(addBlogC)
+    about.classList.add(addAboutC)
 
     lightBulbState.textContent = lightBulbTitle
 }
